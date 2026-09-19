0dc101cdc9bca525a71d3a99934b3d86b4ca0f44
imabsaar007 <imabsaar007@gmail.com>
2026-09-19 19:25:45 +0530

    added  maxSum brute force approach

---
+++ Sliding Window/maxSum.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int maxSum(vector<int> arr,int k){
+    int maxSum = 0;
+    for(int i = 0; i < arr.size(); i++){
+        int sum = 0;
+        for(int j = i; j < i+k; j++){
+            sum +=arr[j]; 
+        }
+        maxSum = max(maxSum,sum);
+    }
+    return maxSum;
+}
+int main(){
+    vector<int> arr = {2,4,5,6,78,3};
+    cout << maxSum(arr,3);
+    return 0;
+}

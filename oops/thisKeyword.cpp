823d57555d3e9453dab2572fa1136b60e9056f18
imabsaar007 <imabsaar007@gmail.com>
2026-09-24 22:38:47 +0530

    added pointer object code

--- oops/thisKeyword.cpp
+++ oops/thisKeyword.cpp
@@ -13,10 +13,19 @@ class FootBaller{
     void print(){
         cout <<name << " " << goals <<" " << age << endl;
     }
+    void change(FootBaller* f,int age){
+        f->age = age;
+    }
 };
 int main(){
     FootBaller f1("Messi",882,39);
     FootBaller f2("Ronaldo",980,41);
+    // using pointer manipulation
+
+    FootBaller* ptr = &f1;
+    cout << (*ptr).goals << endl;
+    (*ptr).goals = 899; // changing the values of the variable using pointer
+    f1.change(&f1,40);
     f1.print();
     f2.print();
 }
